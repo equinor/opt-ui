c1e26ec43358891c458cd5f05c394c9893aa31b5
Adrian Lopez Gonzalez <adrian.gonzalez@bouvet.no>
2023-12-11 16:46:15 +0100

    docs: implement useForceUpdate story

---
+++ packages/opt-ui-core/stories/utils/hooks/useForceUpdate.story.tsx
@@ -0,0 +1,46 @@
+import { Primary, Title } from "@storybook/addon-docs";
+import { useRef } from "react";
+import { Button, useForceUpdate } from "../../../src";
+
+// Base story
+export function Base() {
+  const count = useRef(0);
+  const forceUpdate = useForceUpdate();
+  const handleClick = () => {
+    count.current += 1;
+    forceUpdate();
+  };
+  return (
+    <div>
+      <p>Count: {count.current}</p>
+      <Button onClick={handleClick}>Force Render and update count</Button>
+    </div>
+  );
+}
+
+// Definition
+export default {
+  title: "Utils/Hooks/useForceUpdate",
+  component: useForceUpdate,
+  parameters: {
+    controls: { exclude: ["style"] },
+    docs: {
+      page: () => (
+        <>
+          <Title>useForceUpdate</Title>
+          <p>
+            Returns a function that triggers a component update. the hook
+            equivalent to `this.forceUpdate()` in a class component.
+            <br />
+            <br />
+            In most cases using a state value with `useState` is preferable but
+            this hook can be useful in some advanced usages of refs for interop
+            or when direct DOM manipulation is required.
+          </p>
+          <Primary />
+        </>
+      ),
+    },
+  },
+  tags: ["autodocs"],
+};
