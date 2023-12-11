8e2997aa60870ee2ff96909a8e234d4b26e3b60f
Adrian Lopez Gonzalez <adrian.gonzalez@bouvet.no>
2023-12-11 16:45:48 +0100

    docs: implement useInterval story

---
+++ packages/opt-ui-core/stories/utils/hooks/useInterval.story.tsx
@@ -0,0 +1,38 @@
+import { Primary, Title } from "@storybook/addon-docs";
+import { useState } from "react";
+import { useInterval } from "../../../src";
+
+// Base story
+export function Base() {
+  const [count, setCount] = useState(0);
+  useInterval(() => {
+    setCount((prevCount) => prevCount + 1);
+  }, 1000);
+  return <p>Count: {count}</p>;
+}
+
+// Definition
+export default {
+  title: "Utils/Hooks/useInterval",
+  component: useInterval,
+  parameters: {
+    controls: { exclude: ["style"] },
+    docs: {
+      page: () => (
+        <>
+          <Title>useInterval</Title>
+          <p>
+            Use setInterval in functional React component with the same API.
+            <br />
+            Set your callback function as a first parameter and a delay (in
+            milliseconds) for the second argument. You can also stop the timer
+            passing null instead the delay or even execute it right away passing
+            0.
+          </p>
+          <Primary />
+        </>
+      ),
+    },
+  },
+  tags: ["autodocs"],
+};
