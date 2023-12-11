fdedc36609521ee65044359cce59533b2323c77b
Adrian Lopez Gonzalez <adrian.gonzalez@bouvet.no>
2023-12-11 16:44:40 +0100

    docs: implement useKeyPress story

---
+++ packages/opt-ui-core/stories/utils/hooks/useKeyPress.story.tsx
@@ -0,0 +1,36 @@
+import { Primary, Title } from "@storybook/addon-docs";
+import { useKeyPress } from "../../../src";
+
+// Base story
+export function Base() {
+  const happyPress = useKeyPress("h");
+  return <div>{happyPress ? "😊" : "Press h key"}</div>;
+}
+
+// Definition
+export default {
+  title: "Utils/Hooks/useKeyPress",
+  component: useKeyPress,
+  parameters: {
+    controls: { exclude: ["style"] },
+    docs: {
+      page: () => (
+        <>
+          <Title>useKeyPress</Title>
+          <p>
+            This hook makes it easy to detect when the user is pressing a
+            specific key on their keyboard.
+            <br />
+            <br />
+            <span style={{ color: "red" }}>
+              It doesn&apos;t handle multiple keys being pressed at the same
+              time.
+            </span>
+          </p>
+          <Primary />
+        </>
+      ),
+    },
+  },
+  tags: ["autodocs"],
+};
