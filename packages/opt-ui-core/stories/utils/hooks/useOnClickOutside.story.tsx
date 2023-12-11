c134a62220d2eac051e6fa43c146000433a44a9b
Adrian Lopez Gonzalez <adrian.gonzalez@bouvet.no>
2023-12-11 16:44:02 +0100

    docs: implement useOnClickOutside story

---
+++ packages/opt-ui-core/stories/utils/hooks/useOnClickOutside.story.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable no-alert -- Allowing it for this story */
+import { Primary, Title } from "@storybook/addon-docs";
+import { useRef } from "react";
+import { useOnClickOutside } from "../../../src";
+
+// Base story
+export function Base() {
+  const ref = useRef(null);
+  useOnClickOutside(ref, () => {
+    alert("Click outside box");
+  });
+  return (
+    <div
+      ref={ref}
+      style={{ width: "300px", height: "300px", border: "1px solid black" }}
+    >
+      Click outside this box
+    </div>
+  );
+}
+
+// Definition
+export default {
+  title: "Utils/Hooks/useOnClickOutside",
+  component: useOnClickOutside,
+  parameters: {
+    controls: { exclude: ["style"] },
+    docs: {
+      page: () => (
+        <>
+          <Title>useOnClickOutside</Title>
+          <p>
+            React hook for listening for clicks outside of a specified element.
+            This can be useful for closing a modal, a dropdown menu etc.
+          </p>
+          <Primary />
+        </>
+      ),
+    },
+  },
+  tags: ["autodocs"],
+};
