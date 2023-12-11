5c0d92ad6f1f5d8a561f941ae1a81c982f5038ae
Adrian Lopez Gonzalez <adrian.gonzalez@bouvet.no>
2023-12-11 16:46:42 +0100

    docs: implement useEventCallback story

---
+++ packages/opt-ui-core/stories/utils/hooks/useEventCallback.story.tsx
@@ -0,0 +1,54 @@
+import { Primary, Title } from "@storybook/addon-docs";
+import {
+  Button,
+  useElementSize,
+  useEventCallback,
+  useForceUpdate,
+  usePrevious,
+} from "../../../src";
+
+// Base story
+export function Base() {
+  const forceUpdate = useForceUpdate();
+  const setValue = useEventCallback(() => {
+    forceUpdate();
+  });
+  const previousSetValue = usePrevious(setValue);
+  return (
+    <div>
+      <p>
+        Is callback regenerated:{" "}
+        {setValue !== previousSetValue ? "true" : "false"}
+      </p>
+      <Button onClick={setValue}>Force Render</Button>
+    </div>
+  );
+}
+
+// Definition
+export default {
+  title: "Utils/Hooks/useEventCallback",
+  component: useElementSize,
+  parameters: {
+    controls: { exclude: ["style"] },
+    docs: {
+      page: () => (
+        <>
+          <Title>useEventCallback</Title>
+          <p>
+            A custom React hook that generates a `useCallback` function,
+            ensuring that the callback is only created once and retains the
+            first version of the provided callback function.
+          </p>
+          <p style={{ color: "red" }}>
+            This function is not a replacement for react `useCallback`, the
+            intention of this function is to only generate a function once, on
+            the first render, maintaining its immutability thereafter.
+          </p>
+          <Primary />
+        </>
+      ),
+    },
+  },
+  tags: ["autodocs"],
+};
