74f764d07c1c4dd455a655a6ee6022919f2cf1d1
Adrian Lopez Gonzalez <adrian.gonzalez@bouvet.no>
2023-12-11 16:43:46 +0100

    docs: implement usePrevious story

---
+++ packages/opt-ui-core/stories/utils/hooks/usePrevious.story.tsx
@@ -0,0 +1,40 @@
+import { Primary, Title } from "@storybook/addon-docs";
+import { useState } from "react";
+import { Button, usePrevious } from "../../../src";
+
+// Base story
+export function Base() {
+  const [count, setCount] = useState(0);
+  const prevCount = usePrevious(count);
+  return (
+    <div>
+      <p>{`Now: ${count}, before: ${prevCount}`}</p>
+      <Button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </Button>
+    </div>
+  );
+}
+
+// Definition
+export default {
+  title: "Utils/Hooks/usePrevious",
+  component: usePrevious,
+  parameters: {
+    controls: { exclude: ["style"] },
+    docs: {
+      page: () => (
+        <>
+          <Title>usePrevious</Title>
+          <p>Tracks previous state of a value.</p>
+          <Primary />
+        </>
+      ),
+    },
+  },
+  tags: ["autodocs"],
+};
